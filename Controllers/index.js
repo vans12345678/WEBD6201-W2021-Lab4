15d7a9dea7d65d09fc7bd82033770291a9f93bd2
Andre <andre.agrippa@dcmail.ca>
2021-04-14 22:02:53 -0400

    fixed titles

--- Controllers/index.js
+++ Controllers/index.js
@@ -12,19 +12,19 @@ function DisplayHomePage(req, res, next) {
 }
 exports.DisplayHomePage = DisplayHomePage;
 function DisplayAboutPage(req, res, next) {
-    res.render('index', { title: 'About Us', page: 'about', displayName: index_1.UserDisplayName(req) });
+    res.render('index', { title: 'About Me', page: 'about', displayName: index_1.UserDisplayName(req) });
 }
 exports.DisplayAboutPage = DisplayAboutPage;
 function DisplayServicesPage(req, res, next) {
-    res.render('index', { title: 'Our Services', page: 'services', displayName: index_1.UserDisplayName(req) });
+    res.render('index', { title: 'Services', page: 'services', displayName: index_1.UserDisplayName(req) });
 }
 exports.DisplayServicesPage = DisplayServicesPage;
 function DisplayProjectsPage(req, res, next) {
-    res.render('index', { title: 'Our Projects', page: 'projects', displayName: index_1.UserDisplayName(req) });
+    res.render('index', { title: 'Projects', page: 'projects', displayName: index_1.UserDisplayName(req) });
 }
 exports.DisplayProjectsPage = DisplayProjectsPage;
 function DisplayContactPage(req, res, next) {
-    res.render('index', { title: 'Contact Us', page: 'contact', displayName: index_1.UserDisplayName(req) });
+    res.render('index', { title: 'Contact Me', page: 'contact', displayName: index_1.UserDisplayName(req) });
 }
 exports.DisplayContactPage = DisplayContactPage;
 function DisplayLoginPage(req, res, next) {
